613ddbfbaf7750bd703add47896426b1ddd0d89d
Javi <nhitbh@gmail.com>
2019-03-23 20:01:31 +0100

    Create new class to get public gateways from json file

--- ipfs-status-service/src/main/java/com/fooock/ipfs/status/IpfsStatusServiceApplication.java
+++ ipfs-status-service/src/main/java/com/fooock/ipfs/status/IpfsStatusServiceApplication.java
@@ -2,13 +2,22 @@ package com.fooock.ipfs.status;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
 
 /**
  *
  */
+@EnableScheduling
 @SpringBootApplication
 public class IpfsStatusServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(IpfsStatusServiceApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
     }
 }
