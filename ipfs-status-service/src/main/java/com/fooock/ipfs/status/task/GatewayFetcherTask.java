36337ebde6e5ddb4daaf0ec272bb366cdcb66884
Javi <nhitbh@gmail.com>
2019-03-23 22:52:05 +0100

    Add check before parse response body

--- ipfs-status-service/src/main/java/com/fooock/ipfs/status/task/GatewayFetcherTask.java
+++ ipfs-status-service/src/main/java/com/fooock/ipfs/status/task/GatewayFetcherTask.java
@@ -2,8 +2,8 @@ package com.fooock.ipfs.status.task;
 
 import com.fooock.ipfs.status.model.Gateway;
 import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
@@ -15,7 +15,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- *
+ * This class is used to retrieve the list of public gateways from the official IPFS repository. The list of
+ * gateways is fetched every hour.
  */
 @Slf4j
 @Configuration
@@ -32,23 +33,35 @@ public class GatewayFetcherTask {
         this.jsonParser = jsonParser;
     }
 
-    @Scheduled(fixedRate = 5000)
-    public void getGateways() {
+    /**
+     * Obtain the public list of IPFS gateways. This task is scheduled every hour and the result
+     * is saved to an in-memory database to check one by one its status.
+     */
+    // @Scheduled(cron = "* 0 * * * *")
+    @Scheduled(fixedRate = 10000)
+    public void getPublicGateways() {
         ResponseEntity<String> response = restTemplate.getForEntity(PUBLIC_GATEWAYS_URL, String.class);
+        if (response.getStatusCode().isError()) {
+            log.warn("Error getting response from server, status={}", response.getStatusCodeValue());
+            return;
+        }
         if (response.getBody() == null || response.getBody().isEmpty()) {
-            log.error("Response from {} is not valid (response={})", PUBLIC_GATEWAYS_URL, response.getBody());
+            log.error("Response body from {} is not valid (response={})", PUBLIC_GATEWAYS_URL, response.getBody());
             return;
         }
         try {
-            JsonElement json = jsonParser.parse(response.getBody());
-            JsonArray gatewayArray = json.getAsJsonArray();
-            log.info("Get {} gateways from public repository", gatewayArray.size());
+            List<Gateway> gateways = parseBodyResponse(response.getBody());
+            log.info("Get {} gateways from public repository", gateways.size());
 
-            List<Gateway> gateways = new ArrayList<>(gatewayArray.size());
-            gatewayArray.forEach(element -> gateways.add(new Gateway(element.getAsString())));
-
-        } catch (IllegalStateException e) {
+        } catch (JsonSyntaxException | IllegalStateException e) {
             log.error("Error getting array of gateways from response", e);
         }
+    }
+
+    private List<Gateway> parseBodyResponse(String body) {
+        JsonArray gatewayArray = jsonParser.parse(body).getAsJsonArray();
+        List<Gateway> gateways = new ArrayList<>(gatewayArray.size());
+        gatewayArray.forEach(element -> gateways.add(new Gateway(element.getAsString())));
+        return gateways;
     }
 }
