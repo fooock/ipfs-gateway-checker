7a9fee48b4cdccefd7ffcd7fbe714ea23d03995d
Javi <nhitbh@gmail.com>
2019-03-23 19:47:29 +0100

    First commit

---
+++ ipfs-status-service/src/main/java/com/fooock/ipfs/status/IpfsStatusServiceApplication.java
@@ -0,0 +1,14 @@
+package com.fooock.ipfs.status;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ */
+@SpringBootApplication
+public class IpfsStatusServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IpfsStatusServiceApplication.class, args);
+    }
+}
---
+++ ipfs-status-service/src/test/java/com/fooock/ipfs/status/IpfsStatusServiceApplicationTests.java
@@ -0,0 +1,18 @@
+package com.fooock.ipfs.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ *
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IpfsStatusServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+}
