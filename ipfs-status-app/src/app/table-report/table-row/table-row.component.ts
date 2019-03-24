095cc168577c47b8667548f09565dcc58f6ccd5c
Javi <nhitbh@gmail.com>
2019-03-24 20:48:49 +0100

    Show last update time every second

--- ipfs-status-app/src/app/table-report/table-row/table-row.component.ts
+++ ipfs-status-app/src/app/table-report/table-row/table-row.component.ts
@@ -7,10 +7,22 @@ import {Gateway} from '../gateway';
   styleUrls: ['./table-row.component.css']
 })
 export class TableRowComponent implements OnInit {
+  private currentTime: number;
 
   @Input() gateway: Gateway;
 
   ngOnInit() {
+    setInterval(() => {
+      this.currentTime = new Date().getTime();
+    }, 1000);
   }
 
+  public getLastUpdate(): string {
+    const millis = (this.currentTime - this.gateway.lastUpdate);
+    const seconds = Math.floor(millis / 1000);
+    if (seconds > 60) {
+      return `> ${Math.floor(seconds / 60)} min.`;
+    }
+    return `${seconds} sec.`;
+  }
 }
