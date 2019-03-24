be5efb81fc118e749c183ff072a60e6591004d5f
Javi <nhitbh@gmail.com>
2019-03-24 17:53:54 +0100

    Add bootstrap to web app

--- ipfs-status-app/src/app/app.module.ts
+++ ipfs-status-app/src/app/app.module.ts
@@ -1,7 +1,7 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 
 @NgModule({
   declarations: [
@@ -13,4 +13,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
