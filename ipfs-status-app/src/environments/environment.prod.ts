ce69fe854b34581525d20a4311e70bde04ea3a25
Javi <nhitbh@gmail.com>
2019-03-24 22:12:25 +0100

    Update server port in web app

--- ipfs-status-app/src/environments/environment.prod.ts
+++ ipfs-status-app/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  eventBaseUrl: 'ws://localhost:8080/reports/websocket'
+  eventBaseUrl: 'ws://localhost:8082/reports/websocket'
 };
--- ipfs-status-app/src/environments/environment.ts
+++ ipfs-status-app/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  eventBaseUrl: 'ws://localhost:8080/reports/websocket'
+  eventBaseUrl: 'ws://localhost:8082/reports/websocket'
 };
 
 /*
