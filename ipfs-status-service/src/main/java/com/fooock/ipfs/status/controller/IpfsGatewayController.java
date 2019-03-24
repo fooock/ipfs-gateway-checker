9316bba415cd89e37a318d1c748f153973993ee0
Javi <nhitbh@gmail.com>
2019-03-24 23:16:37 +0100

    Add CrossOrigin annotation to controller class

--- ipfs-status-service/src/main/java/com/fooock/ipfs/status/controller/IpfsGatewayController.java
+++ ipfs-status-service/src/main/java/com/fooock/ipfs/status/controller/IpfsGatewayController.java
@@ -2,6 +2,7 @@ package com.fooock.ipfs.status.controller;
 
 import com.fooock.ipfs.status.model.Report;
 import com.fooock.ipfs.status.repository.ReportMemoryRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.Collection;
 /**
  *
  */
+@CrossOrigin
 @RestController
 @RequestMapping("gateway")
 public class IpfsGatewayController {
