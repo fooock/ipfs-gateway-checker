0186ea50fe1840753aecab7b4a572185fe26ef68
Javi <nhitbh@gmail.com>
2019-12-03 21:11:13 +0100

    Follow redirects when check IPFS node status

--- ipfs-status-service/src/main/java/com/fooock/ipfs/status/IpfsStatusServiceApplication.java
+++ ipfs-status-service/src/main/java/com/fooock/ipfs/status/IpfsStatusServiceApplication.java
@@ -4,8 +4,10 @@ import com.google.gson.JsonParser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
 
 /**
  *
@@ -29,6 +31,8 @@ public class IpfsStatusServiceApplication {
 
     @Bean
     public WebClient getWebClient() {
-        return WebClient.create();
+        return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(HttpClient.create().followRedirect(true)))
+                .build();
     }
 }
